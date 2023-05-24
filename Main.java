5c154071ac8ff3ffdeb1c7573de8b08cb3eb4036
Ariel <arielvincennao@hotmail.com>
2023-05-24 20:54:48 -0300

    Worked on ServicioCaminos.java

--- Main.java
+++ Main.java
@@ -10,10 +10,13 @@ public class Main {
          // Crear un HashMap para los vertices
          HashMap<Integer, ArrayList<Arco<String>>> vertices1 = new HashMap<>();
          HashMap<Integer, ArrayList<Arco<String>>> vertices2 = new HashMap<>();
+         HashMap<Integer, ArrayList<Arco<String>>> vertices3 = new HashMap<>();
+
 
          // Crear un grafo dirigido
          GrafoDirigido<String> grafo1 = new GrafoDirigido<>(vertices1);
          GrafoDirigido<String> grafo2 = new GrafoDirigido<>(vertices2);
+         GrafoDirigido<String> grafo3 = new GrafoDirigido<>(vertices3);
 
  
          // Agregar vértices
@@ -27,7 +30,16 @@ public class Main {
          grafo2.agregarVertice(3);
          grafo2.agregarVertice(4);
          grafo2.agregarVertice(5);
- 
+
+         grafo3.agregarVertice(1);
+         grafo3.agregarVertice(2);
+         grafo3.agregarVertice(3);
+         grafo3.agregarVertice(4);
+         grafo3.agregarVertice(5);
+         grafo3.agregarVertice(6);
+         grafo3.agregarVertice(7);
+         grafo3.agregarVertice(8);
+        
          // Agregar arcos
          grafo1.agregarArco(1, 2, "Arco 1-2");
          grafo1.agregarArco(1, 2, "Arco 1-2"); //Repetido, no se agrega
@@ -41,6 +53,17 @@ public class Main {
          grafo2.agregarArco(1, 2, "Arco 1-2");
          grafo2.agregarArco(3, 4, "Arco 3-4");
          grafo2.agregarArco(4, 5, "Arco 4-5");
+
+         grafo3.agregarArco(1, 4, null);
+         grafo3.agregarArco(1, 3, null);
+         grafo3.agregarArco(1, 2, null);
+         grafo3.agregarArco(4, 8, null);
+         grafo3.agregarArco(4, 3, null);
+         grafo3.agregarArco(3, 7, null);
+         grafo3.agregarArco(3, 6, null);
+         grafo3.agregarArco(2, 3, null);
+         grafo3.agregarArco(2, 6, null);
+         grafo3.agregarArco(2, 5, null);
         
  
      
@@ -53,5 +76,10 @@ public class Main {
          System.out.println("============ BFS ============");
          ServicioBFS bfs = new ServicioBFS(grafo2);
          bfs.bfsForest();
+
+         //Caminos
+         System.out.println("============ Caminos ============");
+         ServicioCaminos sc = new ServicioCaminos(grafo3, 1, 6, 10);
+         sc.caminos();
 
 }}
--- ServicioCaminos.java
+++ ServicioCaminos.java
@@ -1,22 +1,75 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class ServicioCaminos {
-    private Grafo<?> grafo;
+	private Grafo<?> grafo;
 	private int origen;
 	private int destino;
 	private int lim;
-	
+	private HashMap<Integer, String> colores;
+
 	// Servicio caminos
 	public ServicioCaminos(Grafo<?> grafo, int origen, int destino, int lim) {
 		this.grafo = grafo;
 		this.origen = origen;
 		this.destino = destino;
 		this.lim = lim;
+		this.colores = new HashMap<>();
 	}
 
 	public List<List<Integer>> caminos() {
 		// Resolver Caminos
-		return new ArrayList<>();
+		List<List<Integer>> caminosPosibles = new ArrayList<>(); // []
+		List<Integer> caminoActual = new ArrayList<>(); // []
+
+		if (grafo.existeVertice(origen) && this.grafo.existeVertice(destino)) {
+
+			Iterator<Integer> iterator = this.grafo.obtenerVertices();
+			while (iterator.hasNext()) {
+				Integer vertice = iterator.next();
+				colores.put(vertice, "Blanco");
+			}
+
+			dfsCaminos(origen, caminoActual, caminosPosibles);
+
+		} else {
+			System.out.println("El origen o el destino son invalidos");
+		}
+		System.out.println(caminosPosibles);
+		return caminosPosibles;
 	}
+
+	private void dfsCaminos(int vertice, List<Integer> caminoActual, List<List<Integer>> caminosPosibles) {
+
+		if (vertice == destino && caminoActual.size() <= this.lim) {
+			System.out.println("Posible camino encontrado");
+			caminosPosibles.add(new ArrayList<>(caminoActual));
+		} else {
+			System.out.println("Visito " + vertice);
+			colores.put(vertice, "Amarillo");
+			caminoActual.add(vertice);
+
+			Iterator<Integer> iteratorAdyacentes = this.grafo.obtenerAdyacentes(vertice);
+
+			while (iteratorAdyacentes.hasNext()) {
+				Integer clave = iteratorAdyacentes.next();
+
+				if (colores.get(clave) == "Blanco") {
+					dfsCaminos(clave, caminoActual, caminosPosibles);
+				}
+				
+			}
+
+			colores.put(vertice, "Blanco");
+			caminoActual.remove(caminoActual.size() - 1);
+
+
+		}
+
+
+	}
+
+
 }
